5314a99ea5a9330203c0085aa06781da46730e22
Saurav Neve <sauravneve2810@gmail.com>
2024-05-27 18:12:18 +0530

    User Routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const { users } = require("./data/users.json");
+// const { books } = require("./data/books.json");
 
 const app = express();
 
@@ -22,12 +23,124 @@ app.get("/", (req, res) => {
  * Access: Public
  * Parameters: None
  */
-
 app.get("/users", (req, res) => {
   res.status(200).json({
     success: true,
     data: users,
   });
+});
+
+/**
+ * Route: /users/:id
+ * Method: GET
+ * Description: Get single user by their id
+ * Access: Public
+ * Parameters: id
+ */
+app.get("/users/:id", (req, res) => {
+  // const { id } = req.params.id;
+  const { id } = req.params;
+  console.log(req.params);
+  const user = users.find((each) => each.id === id);
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User Doesn't Exist !!",
+    });
+  }
+  return res.status(200).json({
+    success: true,
+    message: "User Found",
+    data: user,
+  });
+});
+
+/**
+ * Route: /users/
+ * Method: POST
+ * Description: Creating a new user
+ * Access: Public
+ * Parameters: none
+ */
+app.post("/users", (req, res) => {
+  const { id, name, surname, email, subscriptionType, subscriptionDate } =
+    req.body;
+
+  const user = users.find((each) => each.id === id);
+
+  if (user) {
+    return res.status(404).json({
+      success: false,
+      message: "User With The ID Exists",
+    });
+  }
+  users.push({
+    id,
+    name,
+    surname,
+    email,
+    subscriptionType,
+    subscriptionDate,
+  });
+
+  return res.status(201).json({
+    success: true,
+    message: "User Added Successfully",
+    data: users,
+  });
+});
+
+/**
+ * Route: /users/:id
+ * Method: PUT
+ * Description: Updating a User by their ID
+ * Access: Public
+ * Parameters: ID
+ */
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { data } = req.body;
+
+  const user = users.find((each) => each.id === id);
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User Doesn't Exist !!",
+    });
+  }
+  const updateUserData = users.map((each) => {
+    if (each.id === id) {
+      return {
+        ...each,
+        ...data,
+      };
+    }
+    return each;
+  });
+  return res.status(200).json({
+    success: true,
+    message: "User Updated !!",
+    data: updateUserData,
+  });
+});
+
+/**
+ * Route: /users/:id
+ * Method: DELETE
+ * Description: Deleting a User by their ID
+ * Access: Public
+ * Parameters: ID
+ */
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.find((each) => each.id === id);
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User Doesn't Exist !!",
+    });
+  }
+  // need to build logic for delete here...
 });
 
 app.get("*", (req, res) => {
